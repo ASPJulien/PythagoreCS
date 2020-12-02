35136735f1a0ef76391bf71f62edaade0756235b
Jupilian <49618271+ASPJulien@users.noreply.github.com>
2020-12-02 16:01:37 +0100

    1.0
    1.0
        + Added Icone
        ~ Changed assembly info

---
+++ Calcul Cote/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Calcul_Cote
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Calculation d'un coté inconnu");
+            Calculate(lengthX(), lengthY(), nameZ());
+
+
+            string nameX()
+            {
+                Console.WriteLine("Donnez le nom de l'hypotenuse");
+                return Console.ReadLine();
+            }
+
+            double lengthX()
+            {
+                Console.WriteLine("Donnez la longueur de " + nameX());
+
+                string LengthX = Console.ReadLine();
+                bool lengthXCorrect = double.TryParse(LengthX, out _);
+
+                if (!lengthXCorrect)
+                {
+                    Console.WriteLine("La longueur n'est pas correcte");
+                    return 0;
+                }
+                else
+                {
+                    return double.Parse(LengthX);
+                }
+            }
+
+            string nameY()
+            {
+                Console.WriteLine("Donnez le nom de la longueur connue \"y\" ");
+                return Console.ReadLine();
+            }
+
+            double lengthY()
+            {
+                Console.WriteLine("Donnez la longueur de " + nameY());
+
+                string LengthY = Console.ReadLine();
+                bool lengthYCorrect = double.TryParse(LengthY, out _);
+
+                if (!lengthYCorrect)
+                {
+                    Console.WriteLine("La longueur n'est pas correcte");
+                    return 0;
+                }
+                else
+                {
+                    return double.Parse(LengthY);
+                }
+            }
+
+            string nameZ()
+            {
+                Console.WriteLine("Donnez le nom de la longueur inconnue \"z\" ");
+                return Console.ReadLine();
+            }
+
+            void Calculate(double _lengthX, double _lengthY, string _nameZ)
+            {
+                _lengthX = Math.Pow(_lengthX, 2);
+                _lengthY = Math.Pow(_lengthY, 2);
+                double lengthZ = _lengthX - _lengthY;
+
+                Console.WriteLine(_nameZ + " est égal à ( racine carrée de ) " + lengthZ);
+                lengthZ = Math.Sqrt(lengthZ);
+                Console.WriteLine(_nameZ + " est ~ égal à " + lengthZ);
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
