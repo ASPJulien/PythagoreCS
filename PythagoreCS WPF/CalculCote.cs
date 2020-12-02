09d00efc70c728118dfbae6bd2081fa16df5ef4f
Jupilian <49618271+ASPJulien@users.noreply.github.com>
2020-12-02 16:08:09 +0100

    2.0
    2.0 
       + Non-Console Interface
       + Rewrited almost all the code

---
+++ PythagoreCS WPF/CalculCote.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+
+namespace PythagoreCS_WPF
+{
+    public static class CalculCote
+    {
+        public static void Calcul(double lengthX, double lengthY, string nameZ)
+        {
+            lengthX = Math.Pow(lengthX, 2);
+            lengthY = Math.Pow(lengthY, 2);
+            double lengthZ = lengthX - lengthY;
+
+            MessageBox.Show(nameZ + " est égal à ( racine carrée de ) " + lengthZ);
+            lengthZ = Math.Sqrt(lengthZ);
+            MessageBox.Show(nameZ + " est ~ égal à " + lengthZ);
+        }
+
+    }
+}
---
+++ PythagoreCS WPF/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PythagoreCS_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private static bool Calcul = true; // true = calcul hypotenuse
+        
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            double lengthX;
+            double lengthY;
+
+            bool isCorrect;
+            isCorrect = double.TryParse(LongeurX.Text, out lengthX);
+            if (isCorrect == false)
+            {
+                MessageBox.Show("Erreur, Merci de recommencer");
+                Environment.Exit(0);                
+            }
+            isCorrect = double.TryParse(LongeurY.Text, out lengthY);
+            if (isCorrect == false)
+            {
+                MessageBox.Show("Erreur, Merci de recommencer");
+                Environment.Exit(0);
+            }
+
+            if (Calcul == true)
+            {
+                CalculHypotenuse.Calcul(lengthX, lengthY, NomZ.Text);
+            }
+            else
+            {
+                CalculCote.Calcul(lengthX, lengthY, NomZ.Text);
+            }
+        }
+
+        private void RadioButton_Checked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Calcul = true;
+        }
+        
+        private void RadioButton_Checked_1(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Calcul = false;
+        }
+        
+    }   
+}
